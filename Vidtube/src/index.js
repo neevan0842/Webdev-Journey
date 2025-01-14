be25be0cc4e5e8e15e5e9be3c9d7e67fafd3eaaa
Naveen Manoj <manojnaveen085@gmail.com>
2025-01-14 19:50:46 +0530

    mongodb connected

--- Vidtube/src/index.js
+++ Vidtube/src/index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
