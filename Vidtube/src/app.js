3023994c74968f84a52a52a3642a55ce890d5360
Naveen Manoj <manojnaveen085@gmail.com>
2025-01-15 16:49:39 +0530

    models done in Vidtube

--- Vidtube/src/app.js
+++ Vidtube/src/app.js
@@ -11,9 +11,11 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 app.use(express.static("public"));
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export default app;
