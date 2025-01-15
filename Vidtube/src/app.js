368a5908f6af83b9870cc08f66bdf19ae4339e91
Naveen Manoj <manojnaveen085@gmail.com>
2025-01-15 20:11:12 +0530

    setup chained methods in userschema

--- Vidtube/src/app.js
+++ Vidtube/src/app.js
@@ -1,18 +1,20 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(
   cors({
     origin: "*",
-    credentials: true,
+    credentials: true, // access-control-allow-credentials
   })
 );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 
