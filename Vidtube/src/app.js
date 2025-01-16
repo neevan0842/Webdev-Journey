6a579415fba69e1bcb3b4427314c9a234111684b
Naveen Manoj <manojnaveen085@gmail.com>
2025-01-16 10:25:57 +0530

    logger added with winston and morgan

--- Vidtube/src/app.js
+++ Vidtube/src/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middlewares.js";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
+const morganFormat = ":method :url :status :response-time ms";
 
 //middleware
 app.use(
@@ -16,6 +19,21 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 //import routes
 import healthCheckRouter from "./routes/healthcheck.routes.js";
