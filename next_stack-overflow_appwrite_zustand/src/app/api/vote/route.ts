2dce8538acf2a1cd8b1366079557a85c532431f1
Naveen Manoj <manojnaveen085@gmail.com>
2025-01-26 12:25:34 +0530

    stackoverflow clone backend done

---
+++ next_stack-overflow_appwrite_zustand/src/app/api/vote/route.ts
@@ -0,0 +1,126 @@
+import {
+  answerCollection,
+  db,
+  questionCollection,
+  voteCollection,
+} from "@/models/name";
+import { databases, users } from "@/models/server/config";
+import { UserPrefs } from "@/store/Auth";
+import { NextRequest, NextResponse } from "next/server";
+import { ID, Query } from "node-appwrite";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { votedbyId, voteStatus, type, typeId } = await request.json();
+
+    const response = await databases.listDocuments(db, voteCollection, [
+      Query.equal("type", type),
+      Query.equal("typeId", typeId),
+      Query.equal("votedbyId", votedbyId),
+    ]);
+
+    if (response.documents.length > 0) {
+      await databases.deleteDocument(
+        db,
+        voteCollection,
+        response.documents[0].$id
+      );
+
+      // decrease the reputation
+      const QuestionOrAnswer = await databases.getDocument(
+        db,
+        type === "question" ? questionCollection : answerCollection,
+        typeId
+      );
+
+      const authorPrefs = await users.getPrefs<UserPrefs>(
+        QuestionOrAnswer.authorId
+      );
+
+      await users.updatePrefs(QuestionOrAnswer.authorId, {
+        reputation:
+          response.documents[0].voteStatus === "upvoted"
+            ? Number(authorPrefs.reputation) - 1
+            : Number(authorPrefs.reputation) + 1,
+      });
+    }
+
+    // it means previous vote doesn't exist or voteStatus has changed
+    if (response.documents[0]?.voteStatus !== voteStatus) {
+      const doc = await databases.createDocument(
+        db,
+        voteCollection,
+        ID.unique(),
+        {
+          type,
+          typeId,
+          voteStatus,
+          votedbyId,
+        }
+      );
+
+      // increase or decrease the reputation
+      const questionOrAnswer = await databases.getDocument(
+        db,
+        type === "question" ? questionCollection : answerCollection,
+        typeId
+      );
+
+      const authorPrefs = await users.getPrefs<UserPrefs>(
+        questionOrAnswer.authorId
+      );
+
+      // if vote was present
+      if (response.documents[0]) {
+        await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+          reputation:
+            // prev vote was upvoted and new vote is downvoted so decrease reputation
+            response.documents[0].voteStatus === "upvoted"
+              ? Number(authorPrefs.reputation) - 1
+              : Number(authorPrefs.reputation) + 1,
+        });
+      } else {
+        await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+          reputation:
+            // prev vote was upvoted and new vote is downvoted so decrease reputation
+            voteStatus === "upvoted"
+              ? Number(authorPrefs.reputation) + 1
+              : Number(authorPrefs.reputation) - 1,
+        });
+      }
+    }
+
+    const [upvotes, downvotes] = await Promise.all([
+      databases.listDocuments(db, voteCollection, [
+        Query.equal("type", type),
+        Query.equal("typeId", typeId),
+        Query.equal("voteStatus", "upvoted"),
+        Query.equal("votedbyId", votedbyId),
+        Query.limit(1),
+      ]),
+      databases.listDocuments(db, voteCollection, [
+        Query.equal("type", type),
+        Query.equal("typeId", typeId),
+        Query.equal("voteStatus", "downvoted"),
+        Query.equal("votedbyId", votedbyId),
+        Query.limit(1),
+      ]),
+    ]);
+
+    return NextResponse.json(
+      {
+        data: {
+          document: null,
+          voteResult: (upvotes.total = downvotes.total),
+        },
+        message: "Vote updated successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error?.message || "Something went wrong" },
+      { status: error?.status || error?.code || 500 }
+    );
+  }
+}
