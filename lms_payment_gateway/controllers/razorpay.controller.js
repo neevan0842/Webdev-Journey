c96c956b2b5ec56c2e4fb2844b2f6d2f30b11f2f
Naveen Manoj <manojnaveen085@gmail.com>
2025-11-01 21:13:28 +0530

    add Razorpay integration for payment processing

---
+++ lms_payment_gateway/controllers/razorpay.controller.js
@@ -0,0 +1,94 @@
+import Razorpay from "razorpay";
+import { CoursePurchase } from "../models/coursePurchase.model.js";
+import { Course } from "../models/course.model.js";
+import crypto from "crypto";
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+export const createRazorpayOrder = async (req, res) => {
+  try {
+    const userId = req.id;
+    const { courseId } = req.body;
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({ error: "Course not found" });
+    }
+    const newPurchase = new CoursePurchase({
+      course: courseId,
+      user: userId,
+      amount: course.price,
+      status: "pending",
+    });
+
+    const options = {
+      amount: course.price * 100, // amount in the smallest currency unit
+      currency: "INR",
+      receipt: "order_rcptid_11",
+      notes: {
+        courseId: courseId,
+        userId: userId,
+      },
+    };
+
+    const orders = await razorpay.orders.create(options);
+
+    newPurchase.paymentId = orders.id;
+    await newPurchase.save();
+
+    return res.status(200).json({
+      success: true,
+      orders,
+      course: {
+        name: course.title,
+        description: course.description,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating Razorpay order:", error);
+  }
+};
+
+export const verifyPayment = async (req, res) => {
+  try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
+
+    // Verify payment signature
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(body.toString())
+      .digest("hex");
+
+    const isAuthentic = expectedSignature === razorpay_signature;
+
+    if (!isAuthentic) {
+      return res.status(400).json({ message: "Payment verification failed" });
+    }
+
+    // Update purchase record
+    const purchase = await CoursePurchase.findOne({
+      paymentId: razorpay_order_id,
+    });
+    if (!purchase) {
+      return res.status(404).json({ message: "Purchase record not found" });
+    }
+
+    purchase.status = "completed";
+    await purchase.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Payment verified successfully",
+      courseId: purchase.course,
+    });
+  } catch (error) {
+    console.error("Error verifying payment:", error);
+    res
+      .status(500)
+      .json({ message: "Error verifying payment", error: error.message });
+  }
+};
